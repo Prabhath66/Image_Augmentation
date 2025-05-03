d3876d8c233344adc2eb36e7f554bb270e763996
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-05-03 13:48:57 +0000

    Update app.py

--- app.py
+++ app.py
@@ -9,24 +9,24 @@ file_uploader=st.file_uploader("Choose an image")
 
 options = ["Translation", "Rotation", "Scaling", "Shearing","Cropping", "Brightness","Grayscale","Flip Horizontally","Flip Vertically"] 
 selection = st.sidebar.pills("Select the options for Image Augmentation", options, selection_mode="multi")
-st.markdown(f"Your selected options: {selection}.")
+st.sidebar.markdown(f"Your selected options: {selection}.")
 
 if file_uploader is not None:
     file_bytes = np.frombuffer(file_uploader.read(), np.uint8)
     img = cv2.imdecode(file_bytes, 1) # 1 is for color image 
-    #st.image(img,  caption="Original Image", channels="BGR")
-    a=img.shape
-    rows,cols = img.shape[0:2]
-    st.write(a,rows,cols)
+    st.image(img,  caption="Original Image", channels="BGR")
+    #a=img.shape
+    #rows,cols = img.shape[0:2]
+    #st.write(a,rows,cols)
 
 
 
-st.write(selection)
+
 for i in selection:
-    st.write(i)
+    #st.write(i)
     if i == "Translation": 
         tx, ty=np.random.randint(-60,60), np.random.randint(-60,60) 
-        st.write(f"Tx: {tx}, Ty={ty}")
+        #st.write(f"Tx: {tx}, Ty={ty}")
         tm=np.array([[1,0,tx],[0,1,ty]],dtype=np.float32) 
         trans_img=cv2.warpAffine(img, tm, dsize=(cols,rows)) 
         st.image(trans_img , caption= "Translation Image",  channels="BGR") 
@@ -34,21 +34,21 @@ for i in selection:
     elif i == "Rotation": 
         center=(cols//2, rows//2)
         angle=np.random.randint(-360,360) 
-        st.write(f"Center:{center}, Angle:{angle}")
+        #st.write(f"Center:{center}, Angle:{angle}")
         rm=cv2.getRotationMatrix2D(center, angle, 1 )
         rotat_img=cv2.warpAffine(img, rm, dsize=(cols,rows))
         st.image(rotat_img , caption="Rotation Image",  channels="BGR" )
 
     elif i == "Scaling": 
         sx, sy =  np.random.uniform(0.3,1.5), np.random.uniform(0.3,1.5) 
-        st.write(f"Sx:{sx}, Sy:{sy}")
+        #st.write(f"Sx:{sx}, Sy:{sy}")
         rm=np.array([[sx,0,0],[0,sy,0]],dtype=np.float32) 
         scale_img=cv2.warpAffine(img, rm, dsize=(cols,rows))
         st.image(scale_img , caption="Scaling Image",  channels="BGR" )
 
     elif i == "Shearing": 
         shx, shy =  np.random.uniform(0,0.35), np.random.uniform(0,0.35) 
-        st.write(f"Shx:{shx}, Sy:{shy}")
+        #st.write(f"Shx:{shx}, Sy:{shy}")
         shm = np.array([[1,shx,0],[shy,1,0]],dtype=np.float32) 
         shear_img=cv2.warpAffine(img, shm, dsize=(cols,rows))
         st.image(shear_img , caption="Shearing Image",  channels="BGR" )
@@ -58,6 +58,7 @@ for i in selection:
         y1 = random.randint(0, rows // 5)
         x2 = random.randint(4 * cols // 5, cols)
         y2 = random.randint(4 * rows // 5, rows)
+        st.write(f"x1,x2=({x1},{x2}) and y1,y2=({y1},{y2})")
         crop_img = img[y1:y2, x1:x2]
         crop_img = cv2.resize(crop_img, (cols, rows)) 
         st.image(crop_img , caption="Cropping Image",  channels="BGR" )
