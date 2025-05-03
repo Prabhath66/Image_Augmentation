5400977c728b1e98a96bbf2011d25ee3528ad9c6
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-05-03 07:48:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ file_uploader=st.file_uploader("Choose an image")
 if file_uploader is not None:
     file_bytes = np.frombuffer(file_uploader.read(), np.uint8)
     img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR) 
-    st.image(img,  caption="Original Image",channels="BGR")
+    #st.image(img,  caption="Original Image",channels="BGR")
     a=img.shape
     x,y = img.shape[0:2]
     st.write(a,x,y)
@@ -15,13 +15,11 @@ selection = st.pills("Select the options for Image Augmentation", options, selec
 st.markdown(f"Your selected options: {selection}.") 
 
 
-# if selection == "Shifting (Translation)": 
-#     img=cv2.imdecode(np.array(file_uploader.getvalue(),dtype=np.unit8))        
-#     img=cv2.resize(img,(500,600)) 
-#     tx=50  
-#     ty=60 
-#     tm=np.array([[1,0,tx],[0,1,ty]],dtype=np.float32) 
-#     trans_img=cv2.warpAffine(img, tm, dsize=(500,600) ) 
-#     st.image([img, trans_img] , captions=["Original Image", "Translation Image"],channels="BGR")
+if selection == "Shifting (Translation)": 
+    tx=50  
+    ty=60 
+    tm=np.array([[1,0,tx],[0,1,ty]],dtype=np.float32) 
+    trans_img=cv2.warpAffine(img, tm, dsize=(x,y) ) 
+    st.image([img, trans_img] , captions=["Original Image", "Translation Image"],channels="BGR")
     
 
