85e0fa49502d89442763f6a4747b603307308cf1
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-05-03 05:51:22 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ import streamlit as st
 
 file_uploader=st.file_uploader("Choose an image") 
 
-if file_uploader is not None:
-    st.write(file_uploader.getvalue())
+options = ["Shifting (Translation)", "Rotation", "Scaling", "Shearing","Cropping"]
+selection = st.pills("Directions", options, selection_mode="multi")
+st.markdown(f"Your selected options: {selection}.")
