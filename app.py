8df5edb66097022c504ed217192eeb2fe469c252
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-05-03 08:16:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import streamlit as st
 import numpy as np 
 import cv2
 
-file_uploader=st.file_uploader("Choose an image") 
+file_uploader=st.file_uploader("Choose an image")  
+
+options = ["Shifting (Translation)", "Rotation", "Scaling", "Shearing","Cropping", "Brightness"] 
+selection = st.pills("Select the options for Image Augmentation", options, selection_mode="multi")
+st.markdown(f"Your selected options: {selection}.")
+
 if file_uploader is not None:
     file_bytes = np.frombuffer(file_uploader.read(), np.uint8)
     img = cv2.imdecode(file_bytes, 1) # 1 is for color image 
@@ -10,13 +15,11 @@ if file_uploader is not None:
     a=img.shape
     x,y = img.shape[0:2]
     st.write(a,x,y)
-# options = ["Shifting (Translation)", "Rotation", "Scaling", "Shearing","Cropping", "Brightness"] 
-# selection = st.pills("Select the options for Image Augmentation", options, selection_mode="multi")
-# st.markdown(f"Your selected options: {selection}.") 
+ 
 
 
     #if selection == "Shifting (Translation)": 
-    tx=50  
+    tx=150  
     ty=60 
     tm=np.array([[1,0,tx],[0,1,ty]],dtype=np.float32) 
     trans_img=cv2.warpAffine(img, tm, dsize=(y,x) ) 
