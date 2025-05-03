819c25eac0ba0388f27d7e1e9ec96155a61c42d8
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-05-03 07:42:51 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,11 @@ import cv2
 file_uploader=st.file_uploader("Choose an image") 
 if file_uploader is not None:
     file_bytes = np.frombuffer(file_uploader.read(), np.uint8)
-    img = cv2.imdecode(file_bytes) 
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR) 
     st.image(img,  caption="Original Image",channels="BGR")
+    a=img.shape
     x,y = img.shape[0:2]
-    st.write(x,y)
+    st.write(a,x,y)
 options = ["Shifting (Translation)", "Rotation", "Scaling", "Shearing","Cropping", "Brightness"] 
 selection = st.pills("Select the options for Image Augmentation", options, selection_mode="multi")
 st.markdown(f"Your selected options: {selection}.") 
