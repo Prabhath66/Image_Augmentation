d80b99f008eba0dcc503f4d3bdfe515e9e74de08
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-05-03 06:15:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ import streamlit as st
 
 file_uploader=st.file_uploader("Choose an image") 
 
-options = ["Shifting (Translation)", "Rotation", "Scaling", "Shearing","Cropping"]
-selection = st.pills("Directions", options, selection_mode="multi")
-st.markdown(f"Your selected options: {selection}.")
+options = ["Shifting (Translation)", "Rotation", "Scaling", "Shearing","Cropping", "Brightness"] 
+selection = st.pills("Select the options for Image Augmentation", options, selection_mode="multi")
+st.markdown(f"Your selected options: {selection}.") 
+
+
+if selection == "Shifting (Translation)": 
+    img=cv2.imread(r"C:\Users\91912\Desktop\Prabhas.jpg")      
+    img=cv2.resize(img,(500,600)) 
+    tx=50  
+    ty=60 
+    tm=np.array([[1,0,tx],[0,1,ty]],dtype=np.float32) 
+    trans_img=cv2.warpAffine(img, tm, dsize=(500,600) ) 
+    cv2.imshow("Original Image", img) 
+    cv2.imshow("Transformed Image", trans_img)
+    cv2.waitKey() 
+    cv2.destroyAllWindows() 
+    
+    
