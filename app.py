fa0716bedb8c1f23bc39d8848d920da9408186a7
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-05-04 07:20:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,58 @@ def rotation(picture):
     rotat_img=cv2.warpAffine(picture, rm, dsize=(cols,rows))
     #st.image(rotat_img , caption="Rotation Image",  channels="BGR" )
     return rotat_img
-                
+
+def scaling(picture):
+    sx, sy =  np.random.uniform(0.3,1.5), np.random.uniform(0.3,1.5) 
+    rm=np.array([[sx,0,0],[0,sy,0]],dtype=np.float32) 
+    scale_img=cv2.warpAffine(img, rm, dsize=(cols,rows))
+    st.image(scale_img , caption="Scaling Image",  channels="BGR" )
+    return scale_img 
+
+def shearing(picture):
+    shx, shy =  np.random.uniform(0,0.35), np.random.uniform(0,0.35) 
+    #st.write(f"Shx:{shx}, Sy:{shy}")
+    shm = np.array([[1,shx,0],[shy,1,0]],dtype=np.float32) 
+    shear_img=cv2.warpAffine(img, shm, dsize=(cols,rows))
+    st.image(shear_img , caption="Shearing Image",  channels="BGR" ) 
+    return shear_img  
+
+def cropping(picture):
+    x1 = random.randint(0, cols // 5)
+    y1 = random.randint(0, rows // 5)
+    x2 = random.randint(4 * cols // 5, cols)
+    y2 = random.randint(4 * rows // 5, rows)
+    crop_img = img[y1:y2, x1:x2]
+    crop_img = cv2.resize(crop_img, (cols, rows)) 
+    st.image(crop_img , caption="Cropping Image",  channels="BGR" )
+    return crop_img
+
+def brightness(picture):
+    bright_arr=np.full((rows,cols,3),np.random.randint(0,157,dtype=np.uint8),)
+    # st.write(bright_arr.shape)  and st.write(img.shape) are equal 
+    if np.random.choice(["add", "subtract"]) == "add": 
+        bright_img=cv2.add(img,bright_arr)
+    else:
+        bright_img=cv2.subtract(img,bright_arr)
+    st.image(bright_img , caption="Brightness Image",  channels="BGR" )
+    return bright_img
+
+def grayscale(picture):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converting img into gray scale with shape (rows,cols)
+    gray_img = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR) # converting gray image into bgr image having 3- with shape (rows,cols,depth)
+    st.image(gray_img , caption="GrayScale Image",  )
+    return gray_img 
+
+
+def flip_horizontally(picture):
+    fliphor_img = cv2.flip(img, 1) 
+    st.image(fliphor_img , caption="Flip Horizontally Image",  channels="BGR" )
+    return fliphor_img
+
+def flip_vertically(picture):
+    flipver_img = cv2.flip(img, 0)
+    st.image(flipver_img , caption="Flip Vertically Image",  channels="BGR" )
+    return flipver_img
 
 file_uploader=st.file_uploader("Choose an image", type=["jpg", "jpeg", "png"])  
 
@@ -47,26 +98,21 @@ if file_uploader is not None:
             
                 if "Translation" in options: 
                     trans_img=translation(img)
-                    st.image(trans_img , caption= "Translation Image",  channels="BGR") 
+                    #st.image(trans_img , caption= "Translation Image",  channels="BGR") 
                     transformed_augment_imgs.append(trans_img)
                     
                 if "Rotation" in options: 
                     rotat_img=rotation(img)
-                    st.image(rotat_img , caption="Rotation Image",  channels="BGR" )
+                    #st.image(rotat_img , caption="Rotation Image",  channels="BGR" )
                     transformed_augment_imgs.append(rotat_img)
                 
                 if "Scaling" in options: 
-                    sx, sy =  np.random.uniform(0.3,1.5), np.random.uniform(0.3,1.5) 
-                    #st.write(f"Sx:{sx}, Sy:{sy}")
-                    rm=np.array([[sx,0,0],[0,sy,0]],dtype=np.float32) 
-                    scale_img=cv2.warpAffine(img, rm, dsize=(cols,rows))
-                    st.image(scale_img , caption="Scaling Image",  channels="BGR" )
+                    scale_img=scaling(img)
+                    #st.image(scale_img , caption="Scaling Image",  channels="BGR" )
                     transformed_augment_imgs.append(scale_img)
             
                 if "Shearing" in options: 
-                    shx, shy =  np.random.uniform(0,0.35), np.random.uniform(0,0.35) 
-                    #st.write(f"Shx:{shx}, Sy:{shy}")
-                    shm = np.array([[1,shx,0],[shy,1,0]],dtype=np.float32) 
+                    
                     shear_img=cv2.warpAffine(img, shm, dsize=(cols,rows))
                     st.image(shear_img , caption="Shearing Image",  channels="BGR" )
                     transformed_augment_imgs.append(shear_img)
