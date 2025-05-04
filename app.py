f421a7806a7dea98e1fa93f6d2f02b3f59de903b
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-05-04 17:19:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -82,8 +82,7 @@ if file_uploader is not None:
         
             for i in range(number_of_images):         
                 img=original_img.copy()
-                if options:
-                    option = np.random.choice(options)
+                option = np.random.choice(options)
                                 
                 if option =="Translation":
                     tx, ty=np.random.randint(-60,60), np.random.randint(-60,60) 
