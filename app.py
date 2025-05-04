66dcf0ccf8635936af8ea1ad36e175c5daf5d968
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-05-04 17:34:06 +0000

    Update app.py

--- app.py
+++ app.py
@@ -144,11 +144,18 @@ if file_uploader is not None:
             
             if st.button("Generate Augmented Images", type="primary"): 
 
+
+                with st.spinner("Generating Images Please wiat...", show_time=True):
+                    time.sleep(5)
+
                 st.subheader("Preview of Few Augumented Images")
                 preview_images=st.columns(4) 
                 for i in range(min(4,len(transformed_augment_imgs))):
-                    image_= cv2.cvtColor(transformed_augment_imgs[i], cv2.COLOR_BGR2RGB)
+                    image_= cv2.cvtColor(transformed_augment_imgs[i], cv2.COLOR_BGR2RGB) 
+                    
                     preview_images[i].image(image_,caption=f"Augmented_Image-{i+1}", use_container_width=True)
+                with st.spinner("Generating Images Please wiat...", show_time=True):
+                    time.sleep(5)
 
                 zip_buffer = io.BytesIO()
                 with zipfile.ZipFile(zip_buffer, "w") as zip_file:
