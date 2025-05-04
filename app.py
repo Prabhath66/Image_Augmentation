769592136d084562a7a03dad5296426d574c5b73
Prabhath Nalla <Prabhath6@users.noreply.huggingface.co>
2025-05-04 07:28:12 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def translation(picture):
     tx, ty=np.random.randint(-60,60), np.random.randint(-60,60) 
     tm=np.array([[1,0,tx],[0,1,ty]],dtype=np.float32) 
     trans_img=cv2.warpAffine(picture, tm, dsize=(cols,rows)) 
-    #st.image(trans_img , caption= "Translation Image",  channels="BGR") 
+    st.image(trans_img , caption= "Translation Image",  channels="BGR") 
     return trans_img 
 
 def rotation(picture):
@@ -15,13 +15,13 @@ def rotation(picture):
     angle=np.random.randint(-180,180) 
     rm=cv2.getRotationMatrix2D(center, angle, 1 )
     rotat_img=cv2.warpAffine(picture, rm, dsize=(cols,rows))
-    #st.image(rotat_img , caption="Rotation Image",  channels="BGR" )
+    st.image(rotat_img , caption="Rotation Image",  channels="BGR" )
     return rotat_img
 
 def scaling(picture):
     sx, sy =  np.random.uniform(0.3,1.5), np.random.uniform(0.3,1.5) 
     rm=np.array([[sx,0,0],[0,sy,0]],dtype=np.float32) 
-    scale_img=cv2.warpAffine(img, rm, dsize=(cols,rows))
+    scale_img=cv2.warpAffine(picture, rm, dsize=(cols,rows))
     st.image(scale_img , caption="Scaling Image",  channels="BGR" )
     return scale_img 
 
@@ -29,7 +29,7 @@ def shearing(picture):
     shx, shy =  np.random.uniform(0,0.35), np.random.uniform(0,0.35) 
     #st.write(f"Shx:{shx}, Sy:{shy}")
     shm = np.array([[1,shx,0],[shy,1,0]],dtype=np.float32) 
-    shear_img=cv2.warpAffine(img, shm, dsize=(cols,rows))
+    shear_img=cv2.warpAffine(picture, shm, dsize=(cols,rows))
     st.image(shear_img , caption="Shearing Image",  channels="BGR" ) 
     return shear_img  
 
@@ -38,7 +38,7 @@ def cropping(picture):
     y1 = random.randint(0, rows // 5)
     x2 = random.randint(4 * cols // 5, cols)
     y2 = random.randint(4 * rows // 5, rows)
-    crop_img = img[y1:y2, x1:x2]
+    crop_img = picture[y1:y2, x1:x2]
     crop_img = cv2.resize(crop_img, (cols, rows)) 
     st.image(crop_img , caption="Cropping Image",  channels="BGR" )
     return crop_img
@@ -47,26 +47,26 @@ def brightness(picture):
     bright_arr=np.full((rows,cols,3),np.random.randint(0,157,dtype=np.uint8),)
     # st.write(bright_arr.shape)  and st.write(img.shape) are equal 
     if np.random.choice(["add", "subtract"]) == "add": 
-        bright_img=cv2.add(img,bright_arr)
+        bright_img=cv2.add(picture,bright_arr)
     else:
-        bright_img=cv2.subtract(img,bright_arr)
+        bright_img=cv2.subtract(picture,bright_arr)
     st.image(bright_img , caption="Brightness Image",  channels="BGR" )
     return bright_img
 
 def grayscale(picture):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converting img into gray scale with shape (rows,cols)
+    gray = cv2.cvtColor(picture, cv2.COLOR_BGR2GRAY) # converting img into gray scale with shape (rows,cols)
     gray_img = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR) # converting gray image into bgr image having 3- with shape (rows,cols,depth)
     st.image(gray_img , caption="GrayScale Image",  )
     return gray_img 
 
 
 def flip_horizontally(picture):
-    fliphor_img = cv2.flip(img, 1) 
+    fliphor_img = cv2.flip(picture, 1) 
     st.image(fliphor_img , caption="Flip Horizontally Image",  channels="BGR" )
     return fliphor_img
 
 def flip_vertically(picture):
-    flipver_img = cv2.flip(img, 0)
+    flipver_img = cv2.flip(picture, 0)
     st.image(flipver_img , caption="Flip Vertically Image",  channels="BGR" )
     return flipver_img
 
@@ -93,70 +93,34 @@ if file_uploader is not None:
             transformed_augment_imgs=[]
             
             for i in range(int(number_of_images/len(options))):
-                img=original_img.copy() 
-        
+                img=original_img.copy()         
             
                 if "Translation" in options: 
-                    trans_img=translation(img)
-                    #st.image(trans_img , caption= "Translation Image",  channels="BGR") 
-                    transformed_augment_imgs.append(trans_img)
+                    transformed_augment_imgs.append(translation(img))
                     
                 if "Rotation" in options: 
-                    rotat_img=rotation(img)
-                    #st.image(rotat_img , caption="Rotation Image",  channels="BGR" )
-                    transformed_augment_imgs.append(rotat_img)
+                    transformed_augment_imgs.append(rotation(img))
                 
-                if "Scaling" in options: 
-                    scale_img=scaling(img)
-                    #st.image(scale_img , caption="Scaling Image",  channels="BGR" )
-                    transformed_augment_imgs.append(scale_img)
+                if "Scaling" in options:
+                    transformed_augment_imgs.append(scaling(img))
             
-                if "Shearing" in options: 
-                    
-                    shear_img=cv2.warpAffine(img, shm, dsize=(cols,rows))
-                    st.image(shear_img , caption="Shearing Image",  channels="BGR" )
-                    transformed_augment_imgs.append(shear_img)
+                if "Shearing" in options:
+                    transformed_augment_imgs.append(shearing(img))
             
                 if "Cropping" in options:
-                    x1 = random.randint(0, cols // 5)
-                    y1 = random.randint(0, rows // 5)
-                    x2 = random.randint(4 * cols // 5, cols)
-                    y2 = random.randint(4 * rows // 5, rows)
-                    st.write(f"x1,x2=({x1},{x2}) and y1,y2=({y1},{y2})")
-                    crop_img = img[y1:y2, x1:x2]
-                    crop_img = cv2.resize(crop_img, (cols, rows)) 
-                    st.image(crop_img , caption="Cropping Image",  channels="BGR" )
-                    transformed_augment_imgs.append(crop_img)
+                    transformed_augment_imgs.append(cropping(img))
             
-                if "Brightness" in options: 
-                    bright_arr=np.full((rows,cols,3),np.random.randint(0,157,dtype=np.uint8),)
-                    # st.write(bright_arr.shape)
-                    # st.write(img.shape)
-                    if np.random.choice(["add", "subtract"]) == "add": 
-                        bright_img=cv2.add(img,bright_arr)
-                    else:
-                        bright_img=cv2.subtract(img,bright_arr)
-                        
-                    st.image(bright_img , caption="Brightness Image",  channels="BGR" )
-                    transformed_augment_imgs.append(bright_img)
-    
+                if "Brightness" in options:
+                    transformed_augment_imgs.append(brightness(img))    
 
                 if "Grayscale" in options:
-                    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # converting img into gray scale with shape (rows,cols)
-                    gray_img = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR) # converting gray image into bgr image having 3- with shape (rows,cols,depth)
-                    st.image(gray_img , caption="GrayScale Image",  )
-                    transformed_augment_imgs.append(gray_img)
+                    transformed_augment_imgs.append(grayscale(img))
             
                 if "Flip Horizontally" in options:
-                    fliphor_img = cv2.flip(img, 1) 
-                    st.image(fliphor_img , caption="Flip Horizontally Image",  channels="BGR" )
-                    transformed_augment_imgs.append(fliphor_img)
+                    transformed_augment_imgs.append(flip_horizontally(img))
             
                 if "Flip Vertically" in options:
-                    flipver_img = cv2.flip(img, 0)
-                    st.image(flipver_img , caption="Flip Vertically Image",  channels="BGR" )
-                    transformed_augment_imgs.append(flipver_img)
-                    
+                    transformed_augment_imgs.append(flip_vertically(img))                    
     
             st.write(len(transformed_augment_imgs))
             
